53f1d8a7d5bf100e53df3ccb0c153c8641ffe1ea
Sonia Amehmbo <87652337+xennia316@users.noreply.github.com>
2025-10-07 02:06:10 +0100

    Time: 8 ms (28.79%) | Memory: 62.1 MB (38.50%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -0,0 +1,29 @@
+function romanToInt(s: string): number {
+    let strArray = s.split("");
+    let answer = 0;
+
+    let romanMap = new Map<string, number>([
+        ["I", 1],
+        ["V", 5],
+        ["X", 10],
+        ["L", 50],
+        ["C", 100],
+        ["D", 500],
+        ["M", 1000],
+    ]);
+
+    for (let i = 0; i < strArray.length ; i++) {
+        let j = i + 1;
+
+        let firstNumber = romanMap.get(strArray[i]);
+        let secondNumber = romanMap.get(strArray[j]);
+
+        if (firstNumber < secondNumber) {
+            answer -= firstNumber;
+        } else {
+            answer += firstNumber;
+        }
+    }
+
+    return answer;
+}
