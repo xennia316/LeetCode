5d4ed54c4e599e3153ed481b5c61133ad8e50df1
Sonia Ajong <87652337+xennia316@users.noreply.github.com>
2025-10-24 13:45:47 -0500

    Time: 0 ms (100.00%) | Memory: 55.5 MB (54.84%) - LeetSync

--- 35-search-insert-position/search-insert-position.ts
+++ 35-search-insert-position/search-insert-position.ts
@@ -1,8 +1,19 @@
 function searchInsert(nums: number[], target: number): number {
-    nums.push(target)
-    nums.sort((a, b) => a - b)
-    let set = new Set(nums)
 
-    console.log(nums, set)
-    return [...set].indexOf(target)
+    let left = 0
+    let right = nums.length - 1
+
+    while (left <= right) {
+
+        let mid = Math.floor((left + right) / 2)
+
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] < target) {
+            left = mid + 1
+        } else if (nums[mid] > target) {
+            right = mid - 1
+        }
+    }
+    return left
 };
